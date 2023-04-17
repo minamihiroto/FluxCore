b0a44bc2061d1d0ff0a2f113be80efd1f2343504
minamihiroto <kokowatamf@gmail.com>
2023-04-17 18:29:47 +0900

    add 404 route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes,Navigate } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from './components/Home';
@@ -21,6 +21,7 @@ const App: React.FC = () => {
         {/* 今の所どちらでも */}
         <Route path="/password-reset" element={<PasswordResetRequest />} />
         <Route path="/password-reset-confirm/:uidb64/:token" element={<PasswordResetConfirm />} />
+        <Route path="*" element={<Navigate to='/404' replace />} />
       </Routes>
     </Router>
   );
