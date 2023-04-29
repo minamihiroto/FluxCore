54f5b910a84034174114b30e20c615d17e3d6a76
minamihiroto <kokowatamf@gmail.com>
2023-04-30 01:42:02 +0900

    Refactor of Breadcrumbs component

--- frontend/src/components/menu/Breadcrumbs.tsx
+++ frontend/src/components/menu/Breadcrumbs.tsx
@@ -23,22 +23,21 @@ const Breadcrumbs: React.FC<Props> = ({ directoryId, documentId, boxId }) => {
   const shouldShowBoxName = boxId || boxId === 0;
 
   useEffect(() => {
-    if (boxId || boxId === 0) {
-      const fetchBoxDetails = async () => {
-        try {
-          const boxDetail = await getBoxDetail(boxId);
-          setBoxName(boxDetail.name);
-        } catch (error) {
-          console.error("Error fetching box details:", error);
-        }
-      };
-
-      fetchBoxDetails();
+    async function fetchBoxDetails() {
+      if (!boxId && boxId !== 0) return;
+      try {
+        const boxDetail = await getBoxDetail(boxId);
+        setBoxName(boxDetail.name);
+      } catch (error) {
+        console.error("Error fetching box details:", error);
+      }
     }
+
+    fetchBoxDetails();
   }, [boxId]);
 
   useEffect(() => {
-    const fetchBreadcrumbs = async () => {
+    async function fetchBreadcrumbs() {
       let newBreadcrumbs: Breadcrumb[] = [];
 
       if (directoryId) {
@@ -48,27 +47,34 @@ const Breadcrumbs: React.FC<Props> = ({ directoryId, documentId, boxId }) => {
       }
 
       setBreadcrumbs(newBreadcrumbs);
-    };
+    }
 
     fetchBreadcrumbs();
   }, [directoryId, documentId]);
+
+  const renderBreadcrumbContent = (
+    breadcrumb: Breadcrumb,
+    isFirst: boolean,
+    isLast: boolean
+  ) => {
+    if (isFirst) {
+      return <Link to={`/box/${breadcrumb.id}`}>{breadcrumb.name}</Link>;
+    } else if (isLast) {
+      return <span>{breadcrumb.name}</span>;
+    } else {
+      return (
+        <Link to={`/directory/${breadcrumb.id}`}>{breadcrumb.name}</Link>
+      );
+    }
+  };
 
   return (
     <nav>
       {breadcrumbs.map((breadcrumb, index) => {
         const isFirst = index === 0;
         const isLast = index === breadcrumbs.length - 1;
+        const content = renderBreadcrumbContent(breadcrumb, isFirst, isLast);
 
-        let content;
-        if (isFirst) {
-          content = <Link to={`/box/${breadcrumb.id}`}>{breadcrumb.name}</Link>;
-        } else if (isLast) {
-          content = <span>{breadcrumb.name}</span>;
-        } else {
-          content = (
-            <Link to={`/directory/${breadcrumb.id}`}>{breadcrumb.name}</Link>
-          );
-        }
         return (
           <span key={breadcrumb.id}>
             {content}
